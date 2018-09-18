bfb0c56f018f94d979773278670dfd306e78e522
Kyohei Oka <0622okakyo@gmail.com>
2018-09-18 14:28:40 +0900

    修正中「

--- attendance/arrangement.py
+++ attendance/arrangement.py
@@ -1,10 +1,29 @@
 import pandas as pd
-import csv
+import csv,os
 import datetime
+import json
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import  MessageEvent, TextMessage, TextSendMessage,FollowEvent
 
+#環境変数取得
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 def write_time(date,name,time,check):
     with open('calculator.csv','a',encoding='UTF-8') as w:
         writer=csv.writer(w)
         writer.writerow([date,name,time,check])
+
+
+@handler.add(MessageEvent)
+def answer_check(event,name,check):
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='{}さんの{}を確認しました。'.format(name,check)))
+
+
+
+    
 
--- attendance/views.py
+++ attendance/views.py
@@ -9,47 +9,48 @@ import datetime
 import time
 from .arrangement import write_time
 
+
 class index(FormView):
     template_name = 'index.html'
     success_url = '/'
 
     def __init__(self):
-        self.today=datetime.datetime.today()
+        self.today = datetime.datetime.today()
         super().__init__()
 
-        self.param={
+        self.param = {
 
-            'message':'フォームに名前、パスワードを入力してください。',
-            'form':Attendance_Form,
-            'model':Attendance_Model.objects.filter(date=datetime.date.today().strftime('%Y-%m-%d'))
+            'message': 'フォームに名前、パスワードを入力してください。',
+            'form': Attendance_Form,
+            'model': Attendance_Model.objects.filter(date=datetime.date.today().strftime('%Y-%m-%d'))
         }
 
     def get(self, request, *args, **kwargs):
-        return render(request,'index.html',self.param)
+        return render(request, 'index.html', self.param)
 
     def post(self, request, *args, **kwargs):
-        user=request.POST['name']
-        password=request.POST['password']
-        check=authenticate(request,username=user,password=password)
+        user = request.POST['name']
+        password = request.POST['password']
+        check = authenticate(request, username=user, password=password)
 
         if check is None:
-            self.param['message']='ユーザー名、またはパスワードが違います。ご確認ください。'
+            self.param['message'] = 'ユーザー名、またはパスワードが違います。ご確認ください。'
         else:
+            
+            #LINEで通知させたほうがいい。
             if not self.param['model'].values('arrival'):
-                
                 user = User.objects.get(username=user)
-                model=Attendance_Model(name=user,arrival=True)
-
-                write_time(datetime.date.today(),user,datetime.datetime.now().strftime('%H:%M:%S'),'出勤')
+                model = Attendance_Model(name=user, arrival=True)
+                write_time(datetime.date.today(), user, datetime.datetime.now().strftime('%H:%M:%S'), '出勤')
                 model.save()
 
-                self.param['message']='{}の出勤を確認しました。'.format(user)
+                self.param['message'] = '{}の出勤を確認しました。'.format(user)
             else:
-                
+
                 user = User.objects.get(username=user)
                 model = Attendance_Model(name=user, arrival=False)
                 write_time(datetime.date.today(), user, datetime.datetime.now().strftime('%H:%M:%S'), '退勤')
                 model.save()
                 self.param['message'] = '{}の退勤を確認しました。お疲れさまでした'.format(user)
 
-        return render(request,'index.html',self.param)
+        return render(request, 'index.html', self.param)
